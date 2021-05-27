bc2489fe457a505aadd588b7b45c31b07808fa0d
Bastien Hubervic-Renard <bastienhr+github@gmail.com>
2021-05-27 16:22:42 +0200

    next_time is setup at character creation with a before_create

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,10 @@
 class Character < ApplicationRecord
   belongs_to :user
   has_many :fight_outputs
+
+  before_create :set_next_time
+
+  def set_next_time
+    self.next_time = Time.now
+  end
 end
