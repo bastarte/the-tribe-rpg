c8df9dc24a1c7d9746d73a2b9490e7ef223abbca
Bastien Hubervic-Renard <bastienhr+github@gmail.com>
2021-05-27 16:22:03 +0200

    added some tests for user and characters

--- test/models/character_test.rb
+++ test/models/character_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class CharacterTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "create incomplete character" do
+    bulbi = Character.new
+    assert_not bulbi.save
+  end
+
+  test "create ok character" do
+    bulbi = Character.new(user: users(:ondine))
+    assert bulbi.save
+  end
+
+  test "character has default next_time" do
+    bulbi = Character.new(user: users(:ondine))
+    bulbi.save
+    assert bulbi.next_time
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "create incomplete user" do
+    ondine = User.new
+    assert_not ondine.save
+  end
+
+  test "create user" do
+    ondine = User.new(email: "ondine@example.org", password: "ondine@example.org")
+    assert ondine.save
+  end
 end
