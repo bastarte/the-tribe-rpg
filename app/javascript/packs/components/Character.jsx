5dbab3958ec5c2af796de6f722c2545d4de4a940
Bastien Hubervic-Renard <bastienhr+github@gmail.com>
2021-06-01 14:23:49 +0200

    display 'Fight' or [Remaining time] on Character card on index

--- app/javascript/packs/components/Character.jsx
+++ app/javascript/packs/components/Character.jsx
@@ -2,9 +2,29 @@ import React from "react";
 
 class Character extends React.Component {
 
+  remainingTimeFormat = () => {
+    const nextTime = new Date(this.props.details.next_time)
+    const currentTime = new Date()
+    const remainingTime = new Date(nextTime - currentTime)
+    return `${remainingTime.getMinutes()}min ${remainingTime.getSeconds()}s`
+  }
+
+  handleRefresh = () => {
+    // by calling this method react re-renders the component
+    this.setState({});
+  };
+
+  componentDidMount() {
+  // Now we need to make it run at a specified interval
+  setInterval(this.handleRefresh, 1000); // runs every second.
+}
+
   render() {
-    console.log(this.props.details);
-    const {name, attack, defense, health, magic, skill, rank, next_time } = this.props.details ;
+    // console.log(this.props.details);
+    const {name, attack, defense, health, magic, skill, rank } = this.props.details ;
+    const nextTime = new Date(this.props.details.next_time) ;
+    const currentTime = new Date()
+    const isAvailable = (currentTime > nextTime)
 
     return (
         <div className="char-card">
@@ -20,12 +40,12 @@ class Character extends React.Component {
               <span className="skill">D: {defense} </span>
               <span className="skill">M: {magic} </span>
               <span className="skill">H: {health} </span>
-              <span className="skill">?: {skill} </span>
+              <span className="skill">🧪: {skill} </span>
             </div>
           </div>
           <div className="char-actions">
             <button type="button" className="btn btn-dark btn-sm btn-block">🖊️Edit</button>
-            <button type="button" className="btn btn-dark btn-sm btn-block">⚡Fight</button>
+            <button type="button" className="btn btn-dark btn-sm btn-block" onClick={this.handleRefresh} disabled={!isAvailable} aria-disabled={!isAvailable}>{isAvailable ? '⚡Fight' : this.remainingTimeFormat()}</button>
           </div>
         </div>
     )
